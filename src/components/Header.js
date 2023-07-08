acf07d89a94c821284459b25b2e88a4a7bf533b6
hitarth revakar <hitarthrevakar4@gmail.com>
2023-07-08 15:15:28 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1 className='header'>RecEord Keeping</h1>
+      <h1 className='header'>Record Keeping</h1>
     </div>
   )
 }
